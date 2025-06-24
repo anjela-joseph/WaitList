892a87494c542b00efcf0698680029daf988e2bf
Anjela Joseph <anjelajoseph621@gmail.com>
2025-06-24 22:41:29 +0530

    urls fixed

--- waitlist_project/urls.py
+++ waitlist_project/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('https://declutterminds-waitlist.onrender.com/waitlist/', include('waitlist.urls')),
+    path('waitlist/', include('waitlist.urls')),
     path('', lambda request: redirect('waitlist/')),
 ]
 if settings.DEBUG:
